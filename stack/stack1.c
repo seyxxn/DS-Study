eb2adf28f62efcc329b5fcde9d1a771637a99129
seyxxn <lucy9480@gmail.com>
2024-01-04 20:42:59 +0900

    스택(정적 배열 사용)

--- stack/stack1.c
+++ stack/stack1.c
@@ -1,58 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_STACK_SIZE 100 /* 스택의 최대 크기 */
 #define TRUE 1
 #define FALSE 0
 #define ERROR_CODE -1
+#define MAX_STACK_SIZE 100
 
-/* 스택 원소 정의 및 초기화 */
 typedef struct
 {
     int key;
-    /* 다른 필드들 */
 }element;
 
 element stack[MAX_STACK_SIZE];
-int top = -1 ; // top이 -1이면 비어있는 상태
+int top = -1;
 
-/*  스택이 비어있는지 검사 */
 int isEmpty(){
     if(top == -1) return TRUE;
+    else return FALSE;        
+}
+
+int isFull(){
+    if(top >= MAX_STACK_SIZE -1) return TRUE;
     else return FALSE;
 }
 
-/* 스택이 가득차있는지 검사 */
-int isFull(){
-    if(top >= MAX_STACK_SIZE - 1)
-        return TRUE;
-    else
-        return FALSE;
-}
-
-/* 스택에 원소 삽입 */
-void push(element item){
-    if(ifFull()) stackFull(); 
-    else    
-        stack[++top] = item;
-}
-
-/* 스택에 원소를 삭제 */
-// 삭제 후 삭제한 원소를 반환하는 함수
-element pop(){
-    if(top == -1) return stackEmtpy();
-    return stack[top--];
-}
-
-/* 스택이 가득 찼을 때 오류 처리 */
 void stackFull(){
     fprintf(stderr, "Stack is full, cannot add element");
-    exit(EXIT_FAILURE);
+    exit(1);
 }
 
-/* 스택이 비어있을 때 오류 처리 */
-element stackEmtpy(){
+
+element stackEmpty(){
     element temp;
     temp.key = ERROR_CODE;
-    fprintf(stderr, "Stack is empty, cannor delete element");
+    fprintf(stderr, "Stakc is empty, cannot delete element");
     return temp;
+}
+
+void push(element item){
+    if(isFull()){
+        stackFull();
+    }else{
+        stack[++top] = item;
+    }
+}
+
+element pop(){
+    if(isEmpty()){
+        return stackEmpty();
+    }return stack[top--];
+}
+
+void printStack(){
+    int i;
+    printf("Stack : ");
+
+    for(i = 0; i <= top; i++){
+        printf("%d ", stack[i].key);
+    }
+    printf("\n");
+}
+
+int main(void){
+    element item1, item2, item3;
+    
+    item1.key = 10;
+    item2.key = 22;
+    item3.key = 55;
+
+    printStack();
+
+    push(item1);
+    printStack();
+
+    push(item2);
+    printStack();
+
+    push(item3);
+    printStack();
+
+    pop();
+    printStack();
+
+    pop();
+    printStack();
+
+    pop();
+    printStack();
+    
+    return 0;
 }
