c1ed4f805e199b0a535969f1ce933ff9d4be84bb
seyxxn <lucy9480@gmail.com>
2024-01-04 15:03:14 +0900

    구조체 정의할 때, typedef 사용

---
+++ ADT/struct2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char name[10];
+    int age;
+    float salaly;
+} humanBeing;
+// 보통은 구조체를 정의할 때, typedef와 함께 사용함
+
+humanBeing person1, person2;
