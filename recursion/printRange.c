ad750d07411c31061a9354cf1294a02f203d3258
seyxxn <lucy9480@gmail.com>
2024-01-08 12:00:14 +0900

    재귀 함수로 구현한 간단한 프로그램 예

---
+++ recursion/printRange.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define noisy(x) (puts(#x), x)
+
+void printRangeIterative(int start, int stop);
+void printRangeRecursive(int start, int stop);
+void printRangeRecursiveReversed(int start, int stop);
+void printRangeRecursiveSplit(int start, int stop);
+
+int main(int argc, char **argv){
+    noisy(printRangeIterative(0,10));
+    noisy(printRangeRecursive(0,10));
+    noisy(printRangeRecursiveReversed(0,10));
+    noisy(printRangeRecursiveSplit(0,10));
+    return 0;
+}
+
+void printRangeIterative(int start, int stop){ // 반복문 사용
+    int i;
+    for(i = start; i < stop; i++){
+        printf("%d\n", i);
+    }
+}
+
+void printRangeRecursive(int start, int stop){ // 재귀 사용
+    if (start < stop){
+        printf("%d\n", start); // 출력 먼저
+        printRangeRecursive(start + 1, stop); // 재귀 나중에 
+    }
+}
+
+void printRangeRecursiveReversed(int start, int stop){ // 재귀 사용
+    if (start < stop){
+        printRangeRecursiveReversed(start+1, stop); // 재귀 먼저
+        printf("%d\n", start); // 출력 나중에
+    }
+}
+
+void printRangeRecursiveSplit(int start, int stop){ // 재귀 사용 (분할 정복 방식 사용)
+    int mid;
+    if(start < stop){
+        mid = (start + stop) / 2;
+        printRangeRecursiveSplit(start, mid);
+        printf("%d\n", mid);
+        printRangeRecursiveSplit(mid+1,stop);
+    }
+}
+
