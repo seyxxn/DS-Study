ce2585db0349c3782f30ca8399bd6d0bd2ed8163
seyxxn <lucy9480@gmail.com>
2024-02-06 20:23:38 +0900

    17298번 오큰수 미해결

---
+++ stack/17298.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+int n;
+
+typedef struct stackNode
+{
+    int data;
+    struct stackNode *link;
+} stackNode;
+
+void push(stackNode **top, int data);
+int pop(stackNode **top);
+int peek(stackNode *top);
+void printStack(stackNode *top);
+void solve(int n, int arr[]);
+
+stackNode *top = NULL;
+
+int main()
+{
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    solve(n, arr);
+
+    return 0;
+}
+
+void push(stackNode **top, int index, int data)
+{
+    stackNode *newNode = (stackNode *)malloc(sizeof(stackNode));
+    newNode->data = data;
+    newNode->link = *top;
+    *top = newNode;
+}
+
+int pop(stackNode **top)
+{
+    if (*top == NULL)
+        return -1;
+    stackNode *deleteNode = *top;
+    int deleteNum = deleteNode->data;
+    *top = (*top)->link;
+    free(deleteNode);
+    return deleteNum;
+}
+
+int peek(stackNode *top)
+{
+    if (top == NULL)
+        return -1;
+    return top->data;
+}
+
+void printStack(stackNode *top)
+{
+    for (; top; top = top->link)
+        printf("%d -> ", top->data);
+    printf("\n");
+}
+
+void solve(int n, int arr[])
+{
+    stackNode *top = NULL;
+    int result[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        /* for (int j = 0; j < n; j++)
+        {
+            while (*top != NULL && peek(&top) < arr[j])
+            {
+                push(&top, arr[j]);
+            }
+            
+            push(&top, arr[j]);
+        } */
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", result[i]);
+    printf("\n");
+}
