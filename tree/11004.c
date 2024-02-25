a5a353cd7968ad24b7172041bce12ac1f3afe0d3
seyxxn <lucy9480@gmail.com>
2024-02-25 21:15:14 +0900

    11004 k번째수 (미해결-정렬 알고리즘으로 풀기)

---
+++ tree/11004.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+int n,k;
+typedef struct minHeap{
+    int *array;
+    int capacity;
+    int size;
+} minHeap;
+
+minHeap *heap;
+
+minHeap * createHeap(int capacity){
+    minHeap *heap = (minHeap *)malloc(sizeof(minHeap));
+    heap->array = (int *)malloc(sizeof(int) * 5000001);
+    heap->capacity = capacity;
+    heap->size = 0;
+    return heap;
+}
+
+int getParentIndex(int index){
+    return (index -1)/2;
+}
+
+void insertHeap(minHeap *heap, int value){
+    if (heap->size >= heap->capacity){
+        return ;
+    }
+
+    int i = heap->size;
+    heap->array[i] = value;
+    heap->size++;
+
+    while(i != 0 && heap->array[i] < heap->array[getParentIndex(i)]){
+        int temp = heap->array[i];
+        heap->array[i] = heap->array[getParentIndex(i)];
+        heap->array[getParentIndex(i)] = temp;
+
+        i = getParentIndex(i);
+    }
+}
+
+int extractHeap(minHeap *heap){
+    if (heap->size <= 0){
+        return -1;
+    }
+
+    int min = heap->array[0];
+    
+    heap->array[0] = heap->array[heap->size - 1];
+    heap->size--;
+
+    int i = 0;
+    while(1){
+        int leftChildIndex = 2 * i + 1;
+        int rightChildIndex = 2 * i + 2;
+        int smallestIndex = i;
+
+        if (leftChildIndex < heap->size && heap->array[i] > heap->array[leftChildIndex]){
+            smallestIndex = leftChildIndex;
+        }
+
+        if (rightChildIndex < heap->size && heap->array[i] > heap->array[rightChildIndex]){
+            smallestIndex = rightChildIndex;
+        }
+
+        if (i == smallestIndex){
+            break;
+        }
+
+        int temp = heap->array[i];
+        heap->array[i] = heap->array[smallestIndex];
+        heap->array[smallestIndex] = temp;
+
+        i = smallestIndex;
+    }
+
+    return min;
+}
+
+
+int main(){
+    scanf("%d %d", &n, &k);
+    heap = createHeap(5000001);
+
+    for(int i = 0; i < n; i++){
+        int num;
+        scanf("%d", &num);
+        insertHeap(heap, num);
+    }
+    
+    for(int i = 1; i <= k; i++){
+        if(i == k){
+            printf("%d", extractHeap(heap));
+        }else{
+            extractHeap(heap);
+        }
+    }
+
+}
